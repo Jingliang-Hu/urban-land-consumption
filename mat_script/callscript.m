6a6afe3a2c6ac630fb2ac9b619aa0d18f11a9f06
jingliang <jingliang.hu@dlr.de>
2019-05-06 17:33:22 +0200

    an example script to call enMIMA_Workflow_One_City.m

--- mat_script/callscript.m
+++ mat_script/callscript.m
@@ -1,6 +1,12 @@
 % directory to data of a city
+% cityPath '/<directory to data>/city'
 cityPath = '/data/hu/sdg/mat_script/data/MUC';
-% directory to fold of scripts
+
+% results will be saved in: cityPath/OUTPUT 
+% e.g. '/data/hu/sdg/mat_script/data/MUC/OUTPUT'
+
+% directory to folder of scripts
+% enviPath = '/<directory to the git local repo>/mat_script'
 enviPath = '/data/hu/sdg/mat_script';
 
 tic;
