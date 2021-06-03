e9f58265211400143d3265750e287454bb745a1d
ga39lev <jingliang.hu@dlr.de>
2021-06-03 23:31:11 +0200

    use nairobi as the example

--- mat_script/example_script.m
+++ mat_script/example_script.m
@@ -3,7 +3,7 @@ enviPath = '.';
 addpath(genpath(enviPath));
 
 % path to data of target city 
-cityPath = './data/LCZ42_22691_Istanbul'
+cityPath = './data/LCZ42_21711_Nairobi'
 
 % run the algorithm
 enMIMA_Workflow_One_City_lowMem(cityPath,enviPath);
